f2279c75c3886becca55c775a2927a4b0ec06f59
aulia rifqi <aulbg99@gmail.com>
2024-08-07 16:09:19 +0700

    feat: keep login dan authguard (#3)

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,17 +1,34 @@
-export const Navbar = () => {
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logoutAction } from "@/redux/slices/userSlice";
+import Link from "next/link";
+
+const Navbar = () => {
+  const { id, name } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
   return (
     <nav className="sticky top-0 bg-transparent">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between py-2">
-          <h1 className="cursor-pointer font-bold">Logo</h1>
-
-          <div className="flex items-center gap-8 font-medium">
+          <h1 className="cursor-pointer text-xl font-bold">Logo</h1>
+          <div className="flex cursor-pointer items-center gap-8 font-medium">
             <h3>Home</h3>
             <h3>Write</h3>
             <h3>Profile</h3>
+            {id ? (
+              <>
+                <Link href="/profile">{name}</Link>
+                <h3 onClick={() => dispatch(logoutAction())}>Logout</h3>
+              </>
+            ) : (
+              <Link href="login">Login</Link>
+            )}
           </div>
         </div>
       </div>
     </nav>
   );
 };
+
+export default Navbar;
--- apps/web/src/hooks/api/auth/useLogin.tsx
+++ apps/web/src/hooks/api/auth/useLogin.tsx
@@ -1,34 +1,44 @@
 "use client";
 
+import useAxios from "@/hooks/useAxios";
 import { axiosInstance } from "@/lib/axios";
+import { useAppDispatch } from "@/redux/hooks";
+import { loginAction } from "@/redux/slices/userSlice";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
 interface LoginPayload {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
 const useLogin = () => {
-    const [isLoading, setIsLoading] = useState(false)
-    const login = async (payload: LoginPayload) => {
-        setIsLoading(true)
-        try {
-            await axiosInstance.post("/api/auth/login", payload);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const router = useRouter()
+  const {axiosInstance} = useAxios()
+  
 
-            toast.success("Login Success")
-         } catch (error) {
-            if(error instanceof AxiosError) {
-                toast.error(error.response?.data || "Something went wrong")
-            }
-            console.log(error)
-         } finally {
-            setIsLoading(false)
-         }
-            
-        }
-        return{login, isLoading};
+  const login = async (payload: LoginPayload) => {
+    setIsLoading(true);
+    try {
+      const { data } = await axiosInstance.post("/api/auth/login", payload);
+      dispatch(loginAction(data));
+      toast("Login Success", { position: "top-left" });
+      router.push("/")
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data || "Something went wrong");
+      }
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
+  };
+
+  return { login, isLoading };
+};
 
 export default useLogin;
