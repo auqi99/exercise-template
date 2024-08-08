72003f7999cbb5e4d34d558ef0948561d3351e8c
aulia rifqi <aulbg99@gmail.com>
2024-08-08 11:39:39 +0700

    feat: forgot password (#4)

---
+++ apps/web/src/app/forgot-password/page.tsx
@@ -0,0 +1,12 @@
+import ForgotPasswordPage from "@/features/forgot-password";
+import React from "react";
+
+const ForgotPassword = () => {
+  return (
+    <>
+      <ForgotPasswordPage />
+    </>
+  );
+};
+
+export default ForgotPassword;
