6eb255a111a702fd3526db23b20b84ce2c91a5da
auqi99 <aulbg99@gmail.com>
2024-08-15 16:40:03 +0700

    feat: blog list

---
+++ apps/api/src/services/blog/get-events.services.ts
@@ -0,0 +1,38 @@
+import prisma from '@/prisma';
+import { PaginationQueryParams } from '@/types/pagination';
+import { Prisma } from '@prisma/client';
+
+interface GetEventsQuery extends PaginationQueryParams {
+  search: string;
+}
+
+export const getEventsService = async (query: GetEventsQuery) => {
+  try {
+    const { page, take, sortBy, sortOrder, search } = query;
+
+    const whereClause: Prisma.BlogWhereInput = {};
+
+    if (search) {
+      whereClause.title = {
+        contains: search,
+      };
+    }
+
+    const events = await prisma.blog.findMany({
+      where: { title: { contains: search } },
+      skip: (page - 1) * take,
+      take: take,
+      orderBy: { [sortBy]: sortOrder },
+      include: { user: { select: { name: true } } },
+    });
+
+    const count = await prisma.blog.count();
+
+    return {
+      data: events,
+      meta: { page, take, total: count },
+    };
+  } catch (error) {
+    throw error;
+  }
+};
--- apps/web/src/features/home/index.tsx
+++ apps/web/src/features/home/index.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import BlogList from "./components/BlogList";
+import Jumbotron from "./components/Jumbotron";
+import { AutoComplete } from "@/components/Autocomplete";
 
 const Homepage = () => {
   const session = useSession();
-  return <div>Hello, {session.data?.user.name}</div>;
+
+  return (
+    <main className="container mx-auto space-y-7 px-4">
+      <Jumbotron />
+      <AutoComplete />
+      <BlogList />
+    </main>
+  );
 };
 
-export default  Homepage;
+export default Homepage;
